e59643d95257611c6a5a2fec49700548473f2d77
Guiillhe <gja.gja@gmail.com>
2022-04-24 05:22:52 -0300

    Projeto-funcionando

---
+++ README.md
@@ -0,0 +1,2 @@
+# DIO-COVID19 
+## Projeto React para saber os dados da covid pelo mundo
---
+++ src/containers/Main/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ContainerStyled = styled.div`
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 2024px;
+  
+
+`
